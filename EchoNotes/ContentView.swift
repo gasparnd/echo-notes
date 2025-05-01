23d93e397ae13244660db16505ff8a7b3122eb05
gasparnd <gaspardolcemascolo@gmail.com>
2025-05-01 10:03:40 -0400

    Initial Commit

---
+++ EchoNotes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EchoNotes
+//
+//  Created by Gaspar Dolcemascolo on 01-05-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EchoNotes/EchoNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  EchoNotesApp.swift
+//  EchoNotes
+//
+//  Created by Gaspar Dolcemascolo on 01-05-25.
+//
+
+import SwiftUI
+
+@main
+struct EchoNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
